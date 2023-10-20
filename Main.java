53c03b5fdac9e73b76d2d2e3c44f82e5633ec97d
Federico W <wuthrich09@gmail.com>
2023-10-19 21:37:51 -0300

    video-09

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) throws IOException{
+        String nombreArchivo = "C:\\Users\\wuthr\\Desktop\\ARGENTINA-PROGRAMA-JAVA\\AP-4.0-2DA\\Libro_prueba.csv";
+
         System.out.println(saludoPersonalizadoPara("Fede" , "Wuthrich" ));
         int[] numeros = {1,5,7,8,10,11};
         int resultado = sumatoria(numeros);
@@ -11,12 +14,19 @@ public class Main {
 
         System.out.println("El promedio es: " + promedio);
 
-        lecturaArchivo();
+        lecturaArchivo(nombreArchivo);
+        escrituraArchivo(nombreArchivo);
     }
 
-    private static void lecturaArchivo(){
-        String nombreArchivo = "C:\\Users\\User\\Desktop\\VATES\\tablas_base.txt\"";
-//        Files.readAllLines(Paths.get(nombreArchivo));
+    private static void lecturaArchivo(String nombreArchivo) throws IOException{
+        for (String unaLinea: Files.readAllLines(Paths.get(nombreArchivo))){
+            System.out.println(unaLinea);
+        }
+    }
+
+    private static void escrituraArchivo (String nombreArchivo) throws IOException {
+        String writeWord = System.lineSeparator() + "Hola mundo";
+        Files.writeString(Paths.get(nombreArchivo), writeWord, StandardOpenOption.APPEND); //StandardOpenOption. APPEND agrega al final de lo que haya en el archivo
     }
 
     private static String saludoPersonalizadoPara(String nombre, String apellido){
